4150b69d8224e4224cb047ed597e7507cab6a304
hallmason17 <hallmason17@gmail.com>
2024-01-09 18:50:58 -0500

    added individual file adds, added subdirs to repo

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ var dirs = new List<string>();
 
 app.AddCommand("dirs", () =>
 {
-    GetSubDirs(Directory.GetDirectories(Directory.GetCurrentDirectory()));
+    GetSubDirs(Directory.GetDirectories(Directory.GetCurrentDirectory(), "*", SearchOption.AllDirectories));
     Console.WriteLine(String.Join("\n", dirs));
 });
 
@@ -23,10 +23,9 @@ void GetSubDirs(string[] path)
 {
     foreach (string subdir in path)
     {
-        if (Directory.GetDirectories(subdir).Length > 0 && !subdir.Contains(".git") && !subdir.Contains(".open-vcs"))
+        if (!subdir.Contains(".git") && !subdir.Contains(".open-vcs"))
         {
             dirs.Add(subdir);
-            GetSubDirs(Directory.GetDirectories(subdir));
         }
     }
 }
@@ -43,67 +42,93 @@ app.AddCommand("init", () =>
     Console.WriteLine(Constants.repoDir);
 });
 
-
-app.AddSubCommand("add", x =>
+app.AddCommand("add", ([Argument] string[] filesToAdd) =>
 {
-    x.AddCommand(".", () =>
+    var files = new List<string>();
+    if (filesToAdd.Length == 0)
     {
-        string[] files = Directory.GetFiles(Constants.currentDir);
-        var indexDict = new Dictionary<string, string>();
-        if (File.Exists(Path.Combine(Constants.repoDir, "index")))
+        Console.WriteLine("Nothing to add");
+    }
+    else if (filesToAdd.Length == 1 && filesToAdd.Contains("."))
+    {
+        foreach (var file in Directory.GetFiles(Constants.currentDir, "*", SearchOption.AllDirectories))
         {
-            indexDict = fileIOService.ReadIndexFile();
-        }
-        foreach (var file in files)
-        {
-            string fileName = file.Substring(Constants.currentDir.Length + 1);
-            long fileSize = file.Length;
-            string fileContents = File.ReadAllText(file);
-            string fileSha = cryptoService.GetSha1(fileContents);
-            if (!indexDict.ContainsKey(fileName) || indexDict[fileName] != fileSha)
+            if (!file.Contains(".git") && !file.Contains(".open-vcs"))
             {
-                Blob blob = new Blob
+                Console.WriteLine(file);
+                files.Add(file);
+            }
+        }
+    }
+    else
+    {
+        foreach (var file in filesToAdd)
+        {
+            string fileToCheck = String.Concat(Constants.currentDir, "/", file);
+            if (!File.Exists(fileToCheck))
+            {
+                Console.WriteLine("File does not exist");
+            }
+            else if (File.Exists(fileToCheck))
+            {
+                files.Add(String.Concat(Constants.currentDir, "/", file));
+            }
+        }
+    }
+    var indexDict = new Dictionary<string, string>();
+    if (File.Exists(Path.Combine(Constants.repoDir, "index")))
+    {
+        indexDict = fileIOService.ReadIndexFile();
+    }
+    foreach (var file in files)
+    {
+        string fileName = file.Substring(Constants.currentDir.Length + 1);
+        long fileSize = file.Length;
+        string fileContents = File.ReadAllText(file);
+        string fileSha = cryptoService.GetSha1(fileContents);
+        if (!indexDict.ContainsKey(fileName) || indexDict[fileName] != fileSha)
+        {
+            Blob blob = new Blob
+            {
+                filePermissions = "100644",
+                type = "blob",
+                origFileName = fileName,
+                sha1 = fileSha,
+                fileName = fileSha == string.Empty ? string.Empty : fileSha.Substring(2, 38),
+                fileContents = compressionService.Compress($"blob {fileSize.ToString()}{fileContents}")
+            };
+
+            if (fileSha != string.Empty)
+            {
+                string dirName = blob.sha1.Substring(0, 2);
+
+                if (!Directory.Exists(Path.Combine(Constants.objectsDir, dirName)))
                 {
-                    filePermissions = "100644",
-                    type = "blob",
-                    origFileName = fileName,
-                    sha1 = fileSha,
-                    fileName = fileSha == string.Empty ? string.Empty : fileSha.Substring(2, 38),
-                    fileContents = compressionService.Compress($"blob {fileSize.ToString()}{fileContents}")
-                };
+                    Directory.CreateDirectory(Path.Combine(Constants.objectsDir, dirName));
+                }
 
-                if (fileSha != string.Empty)
+                indexDict[blob.origFileName] = blob.sha1;
+
+                var fullPath = Path.Combine(Constants.objectsDir, dirName, blob.fileName);
+
+                if (!File.Exists(fullPath))
                 {
-                    string dirName = blob.sha1.Substring(0, 2);
-
-                    if (!Directory.Exists(Path.Combine(Constants.objectsDir, dirName)))
-                    {
-                        Directory.CreateDirectory(Path.Combine(Constants.objectsDir, dirName));
-                    }
-
-                    indexDict[blob.origFileName] = blob.sha1;
-
-                    var fullPath = Path.Combine(Constants.objectsDir, dirName, blob.fileName);
-
-                    if (!File.Exists(fullPath))
-                    {
-                        Console.WriteLine($"Added file: {blob.sha1}");
-                        File.WriteAllBytes(fullPath, blob.fileContents);
-                    }
+                    Console.WriteLine($"Added file: {blob.sha1}");
+                    File.WriteAllBytes(fullPath, blob.fileContents);
                 }
             }
         }
+    }
 
-        foreach (string key in indexDict.Keys.ToList())
+    foreach (string key in indexDict.Keys.ToList())
+    {
+        if (!files.Contains(Path.Combine(Constants.currentDir, key)))
         {
-            if (!files.Contains(Path.Combine(Constants.currentDir, key)))
-            {
-                Console.WriteLine($"Removed file: {key}");
-                indexDict.Remove(key);
-            }
+            Console.WriteLine($"Removed file: {key}");
+            indexDict.Remove(key);
         }
-        fileIOService.SaveIndexFile(indexDict);
-    });
+    }
+    fileIOService.SaveIndexFile(indexDict);
 });
 
 app.AddCommand("commit", () =>
