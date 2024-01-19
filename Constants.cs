0ba500b40ef0655d5e62bee49746a87a997a71b4
hallmason17 <hallmason17@gmail.com>
2024-01-18 19:06:05 -0500

    remove stagingDir

--- Constants.cs
+++ Constants.cs
@@ -5,6 +5,5 @@ public static class Constants
     public const string appDir = "." + appName;
     public static string currentDir = Directory.GetCurrentDirectory();
     public static string repoDir = Directory.GetCurrentDirectory() + "/" + appDir;
-    public static string stagingDir = Directory.GetCurrentDirectory() + "/" + appDir + "/staging/";
-    public static string objectsDir = Directory.GetCurrentDirectory() + "/" + appDir + "/objects/";
+    public static string objectsDir = repoDir + "/objects/";
 }
